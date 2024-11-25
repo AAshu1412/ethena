3c2fcea5ffe7c7c27b475e093ece5f07acb25c90
mujahid002 <mujahidshaik2002@gmail.com>
2024-11-25 21:51:38 +0530

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,32 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.27",
+
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+      timeout: 10000,
+      confirmations: 2,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 10000,
+    },
+  },
 };
